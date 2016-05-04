f68064d894c533df000aede37d9b4375b8a8dc2f
John Serrano <johnserrano404@gmail.com>
2016-05-04 12:58:27 -0700

    expand comment

--- refinery/infernum_one/5-shell_comp.c
+++ refinery/infernum_one/5-shell_comp.c
@@ -1,4 +1,4 @@
-/* compare strings with wildcards*/
+/* compare strings with wildcards. returns 1 if matches, 0 if no match*/
 int shell_comp(char *s1, char *s2) {
   if (*s2 == '*' && *(s2 + 1) == '*') return shell_comp(s1, s2+1);
 
